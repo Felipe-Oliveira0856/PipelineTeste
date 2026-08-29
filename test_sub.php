5789b76d2a85e152d14e31822ac4bd2888898f7f
Felipe-Oliveira0856 <felipeeduardo0856@gmail.com>
2026-08-29 12:10:28 -0300

    Coreção no pipeline e test_sub

--- test_sub.php
+++ test_sub.php
@@ -5,12 +5,12 @@ function subtracao($a, $b) {
 }
 
 $resultado = subtracao(10, 4);
-echo "Testando subtracao de 10 - 4: " . $resultado . "\n";
+echo "10 - 4: " . $resultado . "\n";
 
 if ($resultado === 6) {
-    echo "Teste de subtracao passou com sucesso!\n";
+    echo "Teste foi um sucesso\n";
 
 } else {
-    echo "Erro: O teste de subtracao falhou!\n";
+    echo "O teste de subtracao falhou\n";
 }
 ?>
