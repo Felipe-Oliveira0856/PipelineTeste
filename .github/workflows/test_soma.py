6f08013d61b1ece9c0f70285d9865f9cf6b50dc7
Felipe-Oliveira0856 <felipeeduardo0856@gmail.com>
2026-08-29 11:58:35 -0300

    Correção do arquivo do soma e pipeline yml

--- .github/workflows/test_soma.py
+++ .github/workflows/test_soma.py
@@ -1,7 +1,6 @@
 def soma(a, b):
     return a + b
 
-# Um teste simples para validar a calculadora
-if __name__ == "__main__":
+def test_soma():
     resultado = soma(5, 5)
     print(f"Testando soma de 5 + 5: {resultado}")
